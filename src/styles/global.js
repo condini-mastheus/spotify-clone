d6cfeabdb85315ef9450c8b209cb330ac280238d
condini-mastheus <matheus.condini@gmail.com>
2019-01-28 22:52:53 -0200

    Kicker

---
+++ src/styles/global.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: none;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  body {
+    text-rendering: optimizeLegibility !important;
+    -webkit-font-smothing: antialiased !important;
+    background: #181818;
+    font-family: 'Montserrat', sans-serif;
+    color: #fff;
+  }
+`;
+
+export default GlobalStyles;
